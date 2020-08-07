ca8153f5a62065371ecc9f815596e9c9b914dda3
Arsh Mishra <being_the_best_man@yahoo.com>
2020-08-07 12:22:17 +0530

    my first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'linuxintegration.dart';
+main() {
+  runApp(MyApp());
+
+}
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return myfunc(
+      
+    );
+  }
+}
